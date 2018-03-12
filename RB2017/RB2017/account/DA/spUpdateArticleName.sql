be625a425a76adb766db56a716e3a589762282e4
Ryan Butler <ryanbutler@users.noreply.github.com>
2018-03-12 13:33:24 -0500

    adding three stored procedures to source control

---
+++ RB2017/RB2017/account/DA/spUpdateArticleName.sql
@@ -0,0 +1,37 @@
+Use RB
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE spUpdateArticleName 
+	-- Add the parameters for the stored procedure here
+	@Guid uniqueidentifier,
+	@Article_Name varchar(100)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	UPDATE Contact_Form 
+	Set Article_Name=@Article_Name
+	WHERE Guid=@Guid
+END
+GO
